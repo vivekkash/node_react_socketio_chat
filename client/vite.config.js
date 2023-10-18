194a771bd5e87be13602fb21b12c1b7ca4d73bb2
vivekkash <vivekkashyap97@gmail.com>
2023-10-18 23:56:53 +0530

    pushing to heroku

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,4 +1,4 @@
-import { defineConfig, optimizeDeps } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
@@ -23,7 +23,19 @@ export default defineConfig({
       protocolImports: true,
     }),
   ],
-  optimizeDeps: {
-    exclude: ['react-player/index.js'],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id
+              .toString()
+              .split('node_modules/')[1]
+              .split('/')[0]
+              .toString();
+          }
+        },
+      },
+    },
   },
 });
