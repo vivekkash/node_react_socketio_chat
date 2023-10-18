39e3876884fd9ae70c236cca2481ec79400d96f3
vivekkash <vivekkashyap97@gmail.com>
2023-10-19 01:08:51 +0530

    changed port handle

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const httpServer = createServer(app);
 
 const io = new Server(httpServer, {
   cors: {
-    origin: ['*'],
+    origin: ['http://localhost'],
     methods: ['GET', 'POST'],
   },
 });
